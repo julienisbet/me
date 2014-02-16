046f7470fd9280febc8f7af132cdd36d7092a643
Julie Nisbet <jmnisbet@gmail.com>
2014-02-16 13:51:33 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem 'rack'
+gem 'sass'
+gem 'bourbon'
+gem 'shotgun'
---
+++ public/js/application.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('section[data-type="background"]').each(function(){
+        debugger
+        var $bgobj = $(this); // assigning the object
+     
+        var $window = $(window)
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+             
+            // Put together our final background position
+            var coords = yPos + 'px';
+ 
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });    
+});
