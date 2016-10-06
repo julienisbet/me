d80be3d856f5fc958e3caad0a1774c8ebcfbf2fb
Julie Nisbet <jmnisbet@gmail.com>
2016-10-06 13:33:15 -0700

    adding a bio and more timeline

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,10 +2,6 @@ get '/' do
   erb :index
 end
 
-get '/timeline' do
-  erb :timeline
-end
-
 get '/resume' do
   erb :resume
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,32 @@
 $(document).ready(function(){
+    $(this).scrollTop(0);
     var timelineBlocks = $('.timeline-block'),
         offset = 0.8;
+
+    $('#nav-link-bio').on("click", function(){
+        $('.timeline').addClass("hidden");
+        $('.bio').removeClass("hide");
+        $(this).addClass("selected");
+        $('#nav-link-timeline').removeClass("selected");
+    })
+    $('#nav-link-timeline').on("click", function(){
+        $('.bio').addClass("hide");
+        $('.timeline').removeClass("hidden")
+        $(this).addClass("selected");
+        $('#nav-link-bio').removeClass("selected");
+    })
+
 
     //hide timeline blocks which are outside the viewport
     hideBlocks(timelineBlocks, offset);
 
     //on scolling, show/animate timeline blocks when enter the viewport
     $(window).on('scroll', function(){
-        (!window.requestAnimationFrame) 
-            ? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-            : window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+        if ($('#nav-link-timeline').hasClass('selected')) {
+            (!window.requestAnimationFrame) 
+                ? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+                : window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+        }
     });
 
     function hideBlocks(blocks, offset) {
@@ -20,7 +37,7 @@ $(document).ready(function(){
 
     function showBlocks(blocks, offset) {
         blocks.each(function(){
-            ( $(this).offset().top <= $(window).scrollTop() + 200 + $(window).height()*offset && $(this).find('.timeline-text').hasClass('hidden') ) && $(this).find('.timeline-text').removeClass('hidden').addClass('slide-left');
+            ( $(this).offset().top <= $(window).scrollTop() + 200 + $(window).height()*offset && $(this).find('.timeline-text').hasClass('hidden') ) && $(this).find('.timeline-text').removeClass('hidden').addClass('slide');
         });
     }
 });
